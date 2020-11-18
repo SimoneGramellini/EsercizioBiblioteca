400eda99524acad30c5e642c0d9b6bb250010408
alessandro.torchiani <alessandro.torchiani.stud@ispascalcomandini.it>
2020-11-18 11:03:56 +0100

    Creazione classe libro, inizializzazione attributi, creazione costruttore e proprietà

--- EsercizioBiblioteca/EsercizioBiblioteca/Libro.cs
+++ EsercizioBiblioteca/EsercizioBiblioteca/Libro.cs
@@ -8,5 +8,56 @@ namespace EsercizioBiblioteca
 {
     class Libro
     {
+        //ATTRIBUTI
+        string autore;
+        string titoloLibro;
+        int annoPubblicazione;
+        string editore;
+        int numeroPag;
+
+        //COSTRUTTORE
+        public Libro(string autor, string title, int pubblication, string editor, int pag )
+        {
+            autore = autor;
+            titoloLibro = title;
+            annoPubblicazione = pubblication;
+            editore = editor;
+            numeroPag = pag;
+        }
+
+        //PROPRIETA'
+        public string Autore
+        {
+            get { return autore; }
+            set { autore = value; }
+        }
+
+        public string TitoloLibro
+        {
+            get { return titoloLibro; }
+            set { titoloLibro = value; }
+        }
+
+        public int AnnoPubblicazione
+        {
+            get { return annoPubblicazione; }
+            set { annoPubblicazione = value; }
+        }
+
+        public string Editore
+        {
+            get { return editore; }
+            set { editore = value; }
+        }
+
+        public int NumeroPag
+        {
+            get { return numeroPag; }
+            set { numeroPag = value; }
+        }
+
+
+
     }
+    
 }
