23f6bfa386469d88172276c39ae14b8967e870ab
filippo.omicini <filippo.omicini@PC7316>
2020-11-18 11:00:33 +0100

    Creazione scheletro classe Biblioteca

---
+++ EsercizioBiblioteca/EsercizioBiblioteca/Biblioteca.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EsercizioBiblioteca
+{
+    public class Biblioteca
+    {
+        private string _nome;
+        private string _indirizzo;
+        private DateTime _orario;
+
+        public string Nome
+        {
+            get
+            {
+                
+            }
+            set
+            {
+                
+            }
+        }
+
+        public string Indirizzo
+        {
+            get
+            {
+
+            }
+
+            set
+            {
+            }
+        }
+
+        public DateTime Orari
+        {
+            get
+            {
+
+            }
+            set
+            {
+            }
+        }
+
+        public void AggiungiLibro()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RicercaLibro()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RicercaLibriAutore()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void NumeroLibri()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
